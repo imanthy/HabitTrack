f2423b43d347cb90e85268298a8d86031ff7872c
Anthy Chen <koyun.chen@gmail.com>
2023-06-22 18:16:11 -0700

    Initial commit

---
+++ HabitTrack/HabitTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackApp.swift
+//  HabitTrack
+//
+//  Created by Anthy Chen on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
